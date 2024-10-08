3f1528eb089f425962d1db20bd7f5b6c95215f75
Bruno Silva <brunosilvinosilva@gmail.com>
2024-10-08 16:05:56 +0100

    further object type understanding

--- app.ts
+++ app.ts
@@ -1,7 +1,19 @@
-// Object Types
+// Object Type
 const person = {
   name: "Bruno",
   age: 27,
+  hobbies: ["gaming", "sports"],
 };
 
+// An array of string values
+let favouriteActivities: string[];
+favouriteActivities = ["Football", "Rugby"];
+
 console.log(person.name);
+
+for(const hobby of person.hobbies) {
+  // Detects that hobbies will be an array of strings
+  // can access string functions
+  console.log(hobby.toUpperCase());
+  // hooby.map() would throw an error as it should
+}
