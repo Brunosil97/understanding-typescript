1a6f37f2c565fc767aabec44a3c5db2a7ebaedf6
Bruno Silva <brunosilvinosilva@gmail.com>
2024-10-08 16:00:31 +0100

    object types

--- app.ts
+++ app.ts
@@ -1,12 +1,7 @@
-// A selector parameter shouldn't be used here but just using this method
-// to showcase core types
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  return showResult ? console.log(phrase + n1 + n2) : n1 + n2
-}
+// Object Types
+const person = {
+  name: "Bruno",
+  age: 27,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const phrase = "Result is: ";
-
-add(number1, number2, printResult, phrase);
+console.log(person.name);
---
+++ basics.ts
@@ -0,0 +1,13 @@
+// A selector parameter shouldn't be used here but just using this method
+// to showcase core types
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  return showResult ? console.log(phrase + result) : result;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const phrase = "Result is: ";
+
+add(number1, number2, printResult, phrase);
