442d40a1ccf9731bcc311a2ee7764afb75032e68
Bruno Silva <brunosilvinosilva@gmail.com>
2024-10-09 15:45:40 +0100

    literal types

--- app.ts
+++ app.ts
@@ -1,16 +1,22 @@
 // Union Types
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  conversion: "number" | "text") {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || conversion === "number") {
+    result = +input1 + +input2;
   }
   
   if (typeof input1 === 'string' && typeof input2 === 'string') {
     result = input1.toString() + input2.toString();
   }
+
   return result;
 }
 
-const combineAge = combine(27, 26);
+const combineAge = combine(27, 26, "number");
 
-const combineName = combine("Bruno", "Silva");
+const combineName = combine("Bruno", "Silva", "text");
+
+const combineStringAge = combine("32", "27", "number");
